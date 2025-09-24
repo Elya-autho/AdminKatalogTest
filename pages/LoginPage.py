7d6502e09b500c7f88ab1f1392b3f54f3c6b31c3
Elya-autho <filippova.elvina2@gmail.com>
2025-09-24 11:19:12 +0500

    add f

---
+++ pages/LoginPage.py
@@ -0,0 +1,11 @@
+from pages.BasePage import BasePage
+from selenium.webdriver.common.by import By
+
+import allure
+
+
+
+
+LOGIN_FIELD = (By.XPATH,"//*[@class="q-field__control relative-position row no-wrap text-negative"])
+
+
