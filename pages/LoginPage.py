90542732c41fd26561852bb2adc0c51ada92211e
Elya-autho <filippova.elvina2@gmail.com>
2025-09-25 16:47:07 +0500

    add f

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -4,8 +4,36 @@ from selenium.webdriver.common.by import By
 import allure
 
 
+class LoginPageLocators:
+    LOGIN_FIELD = (By.XPATH,"(//input[@placeholder='Логин'])")
+    PASSWORD_FIELD = (By.XPATH,"(//input[@placeholder='Пароль'])")
+    BUTTON_SEND = (By.XPATH,"(//*[@class='block']")
 
 
-LOGIN_FIELD = (By.XPATH,"//*[@class="q-field__control relative-position row no-wrap text-negative"])
 
+class LoginPageHelper(BasePage):
+    def __init__(self, driver):
+        self.driver = driver
+        self.check_page()
 
+    def check_page(self):
+        with allure.step('Проверяем корректность загрузки страницы'):
+            self.attach_screenshot()
+        self.find_element(LoginPageLocators.LOGIN_FIELD)
+        self.find_element(LoginPageLocators.PASSWORD_FIELD)
+        self.find_element(LoginPageLocators.BUTTON_SEND)
+
+    @allure.step("Заполняем поле логин")
+    def type_login(self, login):
+        self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys(login)
+        self.attach_screenshot()
+
+    @allure.step("Заполняем поле пароля")
+    def type_password(self, password):
+        self.find_element(LoginPageLocators.PASSWORD_FIELD).send_keys(password)
+        self.attach_screenshot()
+
+    @allure.step("Переходим к регистрации")
+    def click_registration(self):
+        self.attach_screenshot()
+        self.find_element(LoginPageLocators.BUTTON_SEND).click()
---
+++ tests/LoginTest.py
@@ -0,0 +1,10 @@
+from core.BaseTest import browser
+from pages.BasePage import BasePage
+from pages.LoginPage import LoginPageHelper
+import allure
+
+BASE_URL = 'https://test-admin.retail-kb.kbapp.ru/login'
+
+LOGIN = "admin"
+PASSWORD = "nanotek24"
+
